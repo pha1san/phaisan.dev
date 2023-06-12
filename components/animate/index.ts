3a8582f32293c1733486f54f328c61b4ff7edc6e
Phaisan Uyarnontruk <nung1234@gmail.com>
2023-06-13 05:56:15 +1200

    remove animation setting

--- components/animate/index.ts
+++
@@ -1,6 +0,0 @@
-// ----------------------------------------------------------------------
-
-export * from './variants';
-
-export { default as MotionViewport } from './MotionViewport';
-export { default as MotionContainer } from './MotionContainer';
