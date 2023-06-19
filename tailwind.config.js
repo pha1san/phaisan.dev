9059433908d827aa16422184d061ee5ced699711
Phaisan Uyarnontruk <nung1234@gmail.com>
2023-06-20 01:19:06 +1200

    ⬆️ update color and  package

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray: {
+          100: "#f8f9fa",
+          200: "#e9ecef",
+          300: "#dee2e6",
+          400: "#ced4da",
+          500: "#adb5bd",
+          600: "#6c757d",
+          700: "#495057",
+          800: "#343a40",
+          900: "#212529",
+        },
+        blue: {
+          300: "#3d8bfd",
+          400: "#6ea8fe",
+        },
+        light: { text: "212529" },
       },
     },
   },
