02f85352ed727591df99889f3ede4c13dc4c3dbf
Phaisan Uyarnontruk <nung1234@gmail.com>
2023-06-22 05:39:46 +1200

    💄 update navbar

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <section className="bg-blue-400 text-white ">
       <div className="mx-auto flex h-[80px] w-full max-w-[1116px] items-center justify-between">
-        <div>Copyright © 2023. All rights are reserved</div>
+        <div>Made with ❤️ (and NextJs) by Phaisan U.</div>
         <div className="flex gap-5">
           <Icon icon="mdi:linkedin" width={32} height={32} />
           <Icon icon="mdi:github" width={32} height={32} />
--- layouts/NavBar.tsx
+++ layouts/NavBar.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 
+import { MenuButton } from "@/components/MenuButton";
 import { navLink } from "@/constants/links";
 
 export default function Navbar() {
   return (
-    <nav className="h-[60px] flex items-center justify-between fixed top-0 inset-x-0 bg-white shadow-[0px_2px_4px_rgba(0,0,0,0.075)] z-[999]">
-      <div className="  flex items-center justify-between max-w-[1116px] w-full mx-auto">
-        <Link href="/">👋 Phaisan.dev</Link>
-        <div className="flex gap-7">
+    <nav className="fixed inset-x-0 top-0 z-[999] flex h-[60px] w-screen items-center justify-between bg-white shadow-[0px_2px_4px_rgba(0,0,0,0.075)]">
+      <div className="mx-auto flex w-full max-w-[1116px] items-center justify-between px-5 md:px-0">
+        <Link href="/" className="text-2xl font-bold">
+          👋 Phaisan.dev
+        </Link>
+        <div className="hidden gap-7 md:flex">
           {navLink.map((link) => (
             <a key={link.herf} href={link.herf} className="font-medium">
               {link.label}
             </a>
           ))}
         </div>
+        <MenuButton className="flex md:hidden" />
       </div>
     </nav>
   );
