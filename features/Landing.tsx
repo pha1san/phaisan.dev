edf3b62c13a2e21009975f03c2a1fce0d07f4dfd
Phaisan Uyarnontruk <nung1234@gmail.com>
2023-06-22 00:29:16 +1200

    add section

--- features/Landing.tsx
+++ features/Landing.tsx
@@ -1,50 +1,29 @@
+"use client";
+
 import React from "react";
 
-import { Text, useColorModeValue, VStack } from "@chakra-ui/react";
-import clsx from "clsx";
-import { BsChevronDown } from "react-icons/bs";
-import Jump from "react-reveal/Jump";
-import { Link } from "react-scroll";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
 
-import LinkIconBar from "@/components/LinkIconBar";
 import SectionContainer from "@/components/SectionContainer";
-import { links } from "@/constants/links";
-// import styles from "../../styles/sections/Landing.module.css";
-import { colors } from "@/theme";
+import SocialLinkButton from "@/components/SocialLinkButton";
 
 export default function Landing() {
-  const primary = useColorModeValue(colors.primary.dark, colors.primary.light);
-  const secondary = useColorModeValue(colors.secondary.light, colors.secondary.dark);
-
-  const header = (
-    <div className="text-center pb-[50px]">
-      <h1 className="text-[2.5em]" style={{ color: primary }}>
-        Hi, I&apos;m Phaisan Uyarnontruk,{" "}
-      </h1>
-      <h1 className="text-[2.5em]" style={{ color: primary }}>
-        {" "}
-        a <strong style={{ color: secondary }}>frontend developer.</strong>
-      </h1>
-    </div>
-  );
-
   return (
-    <SectionContainer id="landing" name="landing" headerText={header}>
-      <LinkIconBar links={links} />
-      <Link activeClass="active" to="about" spy smooth offset={-70} duration={500}>
-        <VStack>
-          <Text
-            as="strong"
-            _hover={{ color: secondary }}
-            className={clsx("font-bold text-[1.25em] pb-5 transition-[all_0.2s_ease-in-out]", "hover:scale-[1.1]")}
-          >
-            Learn More
-          </Text>
-          <Jump forever duration={1500}>
-            <BsChevronDown />
-          </Jump>
-        </VStack>
-      </Link>
+    <SectionContainer className="flex h-full flex-col pt-14">
+      <div className="flex">
+        <div>
+          <h1>
+            <span>Phaisan U.</span> Full-stack Developer
+          </h1>
+          <p>Based in Auckland, New Zealand </p>
+          <SocialLinkButton />
+        </div>
+        <div className="relative w-1/3">
+          <Image src={"/assets/images/landing.png"} alt={""} fill />
+        </div>
+      </div>
+      {/* <div>Tech Stack</div> */}
     </SectionContainer>
   );
 }
