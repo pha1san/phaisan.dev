a8464eeaf9178b23ed392152ada2a08cf72c0db4
Phaisan Uyarnontruk <nung1234@gmail.com>
2023-06-23 04:18:43 +1200

    update header

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -11,7 +11,7 @@ const HeaderBox: FC<ITHeaderBox> = ({ title, desc, className, ...props }) => {
   return (
     <div className={clsx(className, "rounded-[20px] bg-blue-300 p-6 text-white")} {...props}>
       <h2 className="mb-3 text-xl font-semibold md:text-2xl">{title}</h2>
-      <p className="text-base font-medium md:text-xl">{desc}</p>
+      <p className="text-left text-base font-medium md:text-xl">{desc}</p>
     </div>
   );
 };
