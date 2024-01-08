5ac4a17246d40ae9726b8940552625d4f5ab8500
pha1san <42394385+pha1san@users.noreply.github.com>
2024-01-09 03:30:39 +1300

    change img and add animate

--- features/Landing.tsx
+++ features/Landing.tsx
@@ -1,5 +1,8 @@
+"use client";
 import clsx from "clsx";
+import { m } from "framer-motion";
 
+import { varContainer, varFade } from "@/components/animate";
 import SectionContainer from "@/components/SectionContainer";
 import SocialLinkButton from "@/components/SocialLinkButton";
 import TechStacksWrapper from "@/components/TechStacksWrapper";
@@ -7,22 +10,36 @@ import TechStacksWrapper from "@/components/TechStacksWrapper";
 export default function Landing() {
   return (
     <SectionContainer className="flex flex-col justify-center">
-      <div
+      <m.div
         className={clsx(
           "flex flex-col-reverse items-center justify-between gap-20 pt-14",
           "md:flex-row md:items-start md:pt-32"
         )}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={varContainer()}
       >
-        <div>
+        <m.div variants={varFade().inUp}>
           <h1 className="text-3xl font-bold leading-[62px] md:text-5xl">
             <span className="text-blue-400">Phaisan U.</span> <br />
             Frontend Developer
           </h1>
           <p className="mt-2 text-lg text-gray-600 md:text-2xl">Based in Auckland, New Zealand </p>
-          <SocialLinkButton className="mt-5" />
-        </div>
-        <img src="/assets/images/landing.svg" alt={""} className="md:w-[min(45%,420px)]" />
-      </div>
+          <SocialLinkButton className="mt-5 md:mt-28 " />
+        </m.div>
+        {/* <m.img variants={varFade().inUp} src="/assets/images/landing.svg" alt={""} className="md:w-[min(45%,420px)]" /> */}
+        <m.div className="relative w-3/5 md:w-[min(45%,320px)]" variants={varFade().inUp}>
+          <m.img whileHover={{ scale: 1.1 }} src="/me.jpg" alt="Phaisan" className="box-shadow-3 rounded-full" />
+          <m.img
+            src="/assets/icons/picIconSet.svg"
+            alt="icons"
+            className="absolute bottom-0 left-[-22px] z-[-1]"
+            variants={varFade().inRight}
+          />
+          <div className="bg-blur absolute inset-y-0 right-0 z-[-2] w-2/3 translate-x-[-10%] translate-y-[35%] rounded-full" />
+        </m.div>
+      </m.div>
       <TechStacksWrapper />
     </SectionContainer>
   );
