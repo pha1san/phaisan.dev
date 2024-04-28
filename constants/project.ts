59db826f828ac2e297a44e90acc2faefc1f70f0f
pha1san <42394385+pha1san@users.noreply.github.com>
2024-04-28 22:02:11 +1200

    ✅ add project list

---
+++ constants/project.ts
@@ -0,0 +1,50 @@
+export const PROJECT = [
+  {
+    img: "/assets/projects/fwx.png",
+    title: "FWX Website",
+    desc: "FWX's landing page.  A Decentralized Derivatives Platform on EVM chains",
+    link: "https://fwx.finance/",
+  },
+  {
+    img: "/assets/projects/fwx-app.png",
+    title: "FWX DeFi App",
+    desc: "The FWX platform offers two main features: the decentralized derivative exchange (DDEX) and the lending and borrowing pools (LBPs).",
+    link: "https://app.fwx.finance/",
+  },
+  {
+    img: "/assets/projects/fwx-blog.png",
+    title: "FWX Blog",
+    desc: "FWX's company blog, show company related news and updates",
+    link: "https://fwx.finance/blog",
+  },
+  {
+    img: "/assets/projects/fwx-wallet.png",
+    title: "FWX Wallet",
+    desc: "The Standard Non-Custodial Wallet Provide user-centric design interface like mobile banking app and facilitates seamless interaction with FWX DeFi.",
+    link: "https://wallet.fwx.finance",
+  },
+  {
+    img: "/assets/projects/fwx-ims.webp",
+    title: "FWX Internal Monitoring Service",
+    desc: "Monitoring website for DeFi. This site visualize the in-depth statistics for the team and act if there is any high-severity event happen",
+    link: "https://fwx.finance/",
+  },
+  {
+    img: "/assets/projects/fwx-demo.png",
+    title: "FWX Demo App",
+    desc: "FWX's demo decentralized finance app used",
+    link: "https://app.fwx.finance/",
+  },
+  {
+    img: "/assets/projects/intnode.png",
+    title: "Intnode",
+    desc: "Crypto investment advice, insights and anaysis",
+    link: "https://intnode.com/",
+  },
+  {
+    img: "/assets/projects/bookmarks.png",
+    title: "Web Bookmarks",
+    desc: "Personal Project that use JavaScript to create web app that maintains a list of bookmarks",
+    link: "https://bookmarks.phaisan.dev/",
+  },
+];
